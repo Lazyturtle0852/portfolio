1e76d492cfe743dad453ab4e1a4873069e9561ea
Lazyturtle0852 <Kamenogakkou@gmail.com>
2026-03-27 15:36:11 +0900

    react migration done

--- script.js
+++
@@ -1,7 +0,0 @@
-// Tailwind CSSのダークモード設定 (オプション)
-// デフォルトはOSの設定に従う
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-}
